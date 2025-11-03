8b63b80325b28c64efcd0a87584319cd978356b2
Suku8008m <kamapallisukumar@gmail.com>
2025-11-03 23:59:12 +0530

    Fix PORT variable for Render

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Express is rnning !");
+  res.send("Express is running!");
 });
 
 app.listen(PORT, () => {
