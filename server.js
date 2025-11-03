935df238174ea222e558adeffa48f3f5c24bc0d0
Suku8008m <kamapallisukumar@gmail.com>
2025-11-03 23:13:30 +0530

    Initial Render backend

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.Port || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Express is rnning !");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Running on port http://localhost:${PORT}`);
+});
